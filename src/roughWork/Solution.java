034c0eebcca835876ffcd0cee4c44eae072ef3db
ssatyendrak13 <satyendra1es1ds@gmail.com>
2024-03-19 00:13:25 +0530

    print method is added

--- src/roughWork/Solution.java
+++ src/roughWork/Solution.java
@@ -4,7 +4,10 @@ public class Solution {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-		System.out.println("Hello world again");
+		print();
+	}
+	public static void print() {
+		System.out.println("print method is called!");
 	}
 
 }
