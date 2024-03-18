08a68bb0c882b03e5a212b14ad0f204e2ce559e0
ssatyendrak13 <satyendra1es1ds@gmail.com>
2024-03-19 00:10:01 +0530

    intial commit

---
+++ src/roughWork/Solution.java
@@ -0,0 +1,10 @@
+package roughWork;
+
+public class Solution {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		System.out.println("Hello world again");
+	}
+
+}
